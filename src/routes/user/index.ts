9d4ca2070f349adf5e3c00c1fcac79b07cba7aba
Costa <67383500+Costaa18@users.noreply.github.com>
2024-04-10 12:47:30 +0100

    Update userRoutes in index.ts to include deleteUser function

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,5 +1,5 @@
 import { Elysia, t } from "elysia";
-import { getAllUsers, createUser, getUserByID, updateUser } from "./handler";
+import { getAllUsers, createUser, getUserByID, updateUser, deleteUser } from "./handler";
 
 const userRoutes = new Elysia({ prefix: "/user" })
   .get("/", () => getAllUsers())
@@ -8,21 +8,45 @@ const userRoutes = new Elysia({ prefix: "/user" })
   })
   .post("/", ({ body }) => createUser(body), {
     body: t.Object({
-      name: t.String(),
-      email: t.String(),
-      password: t.String(),
+      name: t.String({
+        minLength: 3,
+        maxLength: 15,
+      }),
+      email: t.String({
+        format: "email",
+      }),
+      password: t.String({
+        minLength: 8,
+        maxLength: 20,
+      }),
+      role: t.Optional(
+        t.String({
+          enum: ["OWNER", "EMPLOYEE", "USER", "ADMIN", "SUPER_ADMIN"],
+        })
+      ),
     }),
   })
-  .put("/:id", ({ params: { id }, body }) => updateUser({ ...body, id }), {
+  .put("/:id", ({ params: { id }, body }) => updateUser(id, { ...body }), {
     params: t.Object({ id: t.String() }),
     body: t.Object(
       {
-        name: t.String(),
-        email: t.String(),
-        password: t.String(),
+        name: t.Optional(
+          t.String({
+            minLength: 3,
+            maxLength: 15,
+          })
+        ),
+        email: t.Optional(
+          t.String({
+            format: "email",
+          })
+        ),
       },
       { minProperties: 1 }
     ),
+  })
+  .delete("/:id", ({ params: { id } }) => deleteUser(id), {
+    params: t.Object({ id: t.String() }),
   });
 
 export default userRoutes;
