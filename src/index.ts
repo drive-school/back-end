6cba878ebebad8b2a72507c5879cc8c359175abf
Costa <67383500+Costaa18@users.noreply.github.com>
2024-04-10 12:15:58 +0100

    Add CORS middleware to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Elysia } from "elysia";
 import { swagger } from "@elysiajs/swagger";
 import userRoutes from "./routes/user";
+import { cors } from "@elysiajs/cors";
 
 const app = new Elysia()
+  .use(cors())
   .use(userRoutes)
   .use(swagger())
   .listen(Bun.env.API_PORT || 9696);
