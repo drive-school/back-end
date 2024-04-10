f9462a54db704bb755e407c1d8e42ece839d5c0f
Costa <67383500+Costaa18@users.noreply.github.com>
2024-04-10 12:16:03 +0100

    Add getUserByID and updateUser functions to user handler

--- src/routes/user/handler.ts
+++ src/routes/user/handler.ts
@@ -10,18 +10,57 @@ export async function getAllUsers() {
   }
 }
 
+export async function getUserByID(id: string) {
+  try {
+    const user = await prisma.user.findFirst({ where: { id } });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    return user;
+  } catch (e) {
+    console.log(e);
+    throw new Error("Error fetching user");
+  }
+}
+
 export async function createUser(body: { name: string; email: string; password: string }) {
-  console.log(body);
   if (typeof body === "object" && body !== null) {
     try {
       const { name, email, password } = body;
+
+      const EmailAlreadyExists = await prisma.user.findFirst({ where: { email } });
+      if (EmailAlreadyExists) {
+        throw new Error("Email already exists");
+      }
+
       return await prisma.user.create({ data: { name, email, password } }).finally(() => prisma.$disconnect());
     } catch (e) {
       console.log(e);
       throw new Error("Error creating user");
     }
   } else {
-    console.log(body);
+    throw new Error("Body must be an object");
+  }
+}
+
+export async function updateUser(id: string, body: { name: string; email: string; password: string }) {
+  if (typeof body === "object" && body !== null) {
+    try {
+      const { name, email, password } = body;
+
+      const user = await prisma.user.findFirst({ where: { id } });
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      return await prisma.user
+        .update({ where: { id }, data: { name, email, password } })
+        .finally(() => prisma.$disconnect());
+    } catch (e) {
+      console.log(e);
+      throw new Error("Error updating user");
+    }
+  } else {
     throw new Error("Body must be an object");
   }
 }
