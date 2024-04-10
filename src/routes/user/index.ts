f4f5c04b6fb5514511d6c70930d6694d2f13acd8
Costa <67383500+Costaa18@users.noreply.github.com>
2024-04-10 12:16:08 +0100

    Update userRoutes in index.ts to include getUserByID and updateUser functions

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,15 +1,28 @@
 import { Elysia, t } from "elysia";
-import { getAllUsers, createUser } from "./handler";
+import { getAllUsers, createUser, getUserByID, updateUser } from "./handler";
 
 const userRoutes = new Elysia({ prefix: "/user" })
   .get("/", () => getAllUsers())
+  .get("/:id", ({ params: { id } }) => getUserByID(id), {
+    params: t.Object({ id: t.String() }),
+  })
   .post("/", ({ body }) => createUser(body), {
-    // Changed this line
     body: t.Object({
       name: t.String(),
       email: t.String(),
       password: t.String(),
     }),
+  })
+  .put("/:id", ({ params: { id }, body }) => updateUser({ ...body, id }), {
+    params: t.Object({ id: t.String() }),
+    body: t.Object(
+      {
+        name: t.String(),
+        email: t.String(),
+        password: t.String(),
+      },
+      { minProperties: 1 }
+    ),
   });
 
 export default userRoutes;
