3c14fb3882b76df3be2ba36e1b2090c1a12ed91a
Costa <67383500+Costaa18@users.noreply.github.com>
2024-04-11 19:51:49 +0100

    Add stripeRoutes to index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import { Elysia } from "elysia";
 import { swagger } from "@elysiajs/swagger";
 import userRoutes from "./routes/user";
 import { cors } from "@elysiajs/cors";
+import stripeRoutes from "./routes/stripe";
 
 const app = new Elysia()
   .use(cors())
+  .get("/", async ({ set }) => {
+    //await new Promise((resolve) => setTimeout(resolve, 1000));
+    set.redirect = "/swagger";
+  })
   .use(userRoutes)
+  .use(stripeRoutes)
   .use(swagger())
   .listen(Bun.env.API_PORT || 9696);
 
